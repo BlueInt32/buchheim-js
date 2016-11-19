99a5d9fcd29127bdd36e0a85c6d3bcf9ae7dedd4
Simon Budin <simon.budin@transparency-one.com>
2016-11-19 23:05:22 +0100

    Implement working algorithm

--- service.js
+++ service.js
@@ -2,9 +2,9 @@ function graphDisplayService() {
   var context = {},
     nodes = [], // nodes data provided by API
     links = [], // links data provided by API
-    tree = {},
     maxDepth = 0,
-    nexts = [];
+    nexts = [],
+    distance = 1;
 
   context.nodes = function (_) {
     if (!arguments.length) return nodes;
@@ -19,108 +19,225 @@ function graphDisplayService() {
   };
 
   context.layout = function (autoscale) {
-    resetNodes();
-    computeNodeLinks();
+    prepareNodes();
+    prepareLinks();
     var rootNode = nodes.find(d => d.style === 'root');
-    tree = initTree(rootNode);
-    tree = setup(tree);
-    tree = addmods(tree);
+    var r = convertToTree(rootNode);
+    firstWalk(r);
+    secondWalk(r, r.prelimX);
     return context;
   };
 
-  function resetNodes() {
+  function convertToTree(node, depth, parent, familyNumber) {
+    if (typeof depth === 'undefined') depth = 0;
+    if (typeof familyNumber === 'undefined') familyNumber = 1;
+    var i = 1;
+    var tree = {
+      id: node.masterId,
+      mod: 0,
+      shift: 0,
+      change: 0,
+      thread: null,
+      y: depth,
+      children: [],
+      parent: parent,
+      leftMostSibling: null,
+      number: familyNumber,
+      data: {
+        id: node.masterId,
+        nodeIndex: node.nodeIndex
+      }
+    };
+
+    setNodeY(tree, depth);
+    tree.ancestor = tree;
+    node.outgoingLinks.forEach(l => {
+      tree.children.push(convertToTree(l.target, depth + 1, tree, i));
+      i++;
+    });
+    return tree;
+  }
+
+  function firstWalk(v) {
+    // a bottom up pass to compute a preliminary x value for all the nodes
+    if (v.children.length === 0) { // if v is a leaf
+      if (getLeftMostSibling(v)) {
+        v.prelimX = getLeftSibling(v).prelimX + distance;
+      } else {
+        v.prelimX = 0;
+      }
+    } else {
+      var defaultAncestor = v.children[0]; // defaultAncestor is the leftmost child of v
+      var w = null;
+      for (var i = 0; i < v.children.length; i++) {
+        w = v.children[i];
+        firstWalk(w);
+        defaultAncestor = apportion(w, defaultAncestor);
+      }
+      executeShifts(v);
+      // midpoint is at the middle of leftmost and rightmost children of v
+      var midpoint = (1 / 2) * (v.children[0].prelimX + v.children[v.children.length - 1].prelimX);
+      w = getLeftSibling(v);
+      if (w) {
+        v.prelimX = w.prelimX + distance;
+        v.mod = v.prelimX - midpoint;
+      } else {
+        v.prelimX = midpoint;
+      }
+    }
+  }
+
+  function apportion(v, defaultAncestor) {
+    var w = getLeftSibling(v);
+    if (w) {
+      var vri = v;
+      var vro = v;
+      var vli = w;
+      var vlo = getLeftMostSibling(v);
+      sri = vri.mod;
+      sro = vro.mod;
+      sli = vli.mod;
+      slo = vlo.mod;
+      while (nextRight(vli) && nextLeft(vri)) {
+        vli = nextRight(vli);
+        vri = nextLeft(vri);
+        vlo = nextLeft(vlo);
+        vro = nextRight(vro);
+        vro.ancestor = v;
+        var shift = vli.prelimX + sli - (vri.prelimX + sri) + distance;
+        if (shift > 0) {
+          moveSubtree(ancestor(vli, v, defaultAncestor), v, shift);
+          sri = sri + shift;
+          sro = sro + shift;
+        }
+        sli = sli + vli.mod;
+        sri = sri + vri.mod;
+        slo = slo + vlo.mod;
+        sro = sro + vro.mod;
+      }
+      if (nextRight(vli) && !nextRight(vro)) {
+        vro.thread = nextRight(vli);
+        vro.mod = vro.mod + sli - sro;
+      }
+      if (nextLeft(vri) && !nextLeft(vlo)) {
+        vlo.thread = nextLeft(vri);
+        vlo.mod = vlo.mod + sri - slo;
+        defaultAncestor = v;
+      }
+    }
+    return defaultAncestor;
+  }
+
+  function moveSubtree(wl, wr, shift) {
+    var substrees = wr.number - wl.number;
+    wr.change = wr.change - shift / substrees;
+    wr.shift = wr.shift + shift;
+    wl.change = wl.change + shift / substrees;
+    wr.prelimX = wr.prelimX + shift;
+    wr.mod = wr.mod + shift;
+  }
+
+  function executeShifts(v) {
+    var shift = 0;
+    var change = 0;
+    var w = null;
+    for (var i = v.children.length - 1; i >= 0; i--) {
+      w = v.children[i];
+      w.prelimX = w.prelimX + shift;
+      w.mod = w.mod + shift;
+      change = change + w.change;
+      shift = shift + w.shift + change;
+    }
+  }
+
+  function ancestor(vli, v, defaultAncestor) {
+    if (areSiblings(vli.ancestor, v)) {
+      return vli.ancestor;
+    } else {
+      return defaultAncestor;
+    }
+  }
+
+  function secondWalk(v, m) {
+    v.x = v.prelimX + m;
+    setNodeX(v, v.x);
+    for (var i = 0; i < v.children.length; i++) {
+      secondWalk(v.children[i], m + v.mod);
+    }
+  }
+
+  function areSiblings(v, w) {
+    for (var i = 0; i < v.parent.children.length; i++) {
+      if (areEqualNodes(w, v.parent.children[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function setNodeX(v, x) {
+    nodes[v.data.nodeIndex].x = x;
+  }
+
+  function setNodeY(v, y) {
+    nodes[v.data.nodeIndex].y = y;
+  }
+
+  function nextLeft(v) {
+    if (v.children.length !== 0) {
+      return v.children[0];
+    } else {
+      return v.thread;
+    }
+  }
+
+  function nextRight(v) {
+    if (v.children.length !== 0) {
+      return v.children[v.children.length - 1];
+    } else {
+      return v.thread;
+    }
+  }
+
+  function getLeftSibling(v) {
+    var w = null;
+    var c = null;
+    if (v.parent) {
+      for (var i = 0; i < v.parent.children.length; i++) {
+        c = v.parent.children[i];
+        if (areEqualNodes(c, v)) {
+          return w;
+        } else {
+          w = c;
+        }
+      }
+    }
+    return w;
+  }
+
+  function getLeftMostSibling(v) {
+    if (!v.leftMostSibling && v.parent && !areEqualNodes(v, v.parent.children[0])) {
+      v.leftMostSibling = v.parent.children[0];
+    }
+    return v.leftMostSibling;
+  }
+
+  function prepareNodes() {
     var nodeIndex = 0;
     nodes.forEach(function (node) {
       node.nodeIndex = nodeIndex;
       node.outgoingLinks = [];
       node.incomingLinks = [];
-      node.y = null;
-      node.value = 1;
-      node.dx = config.nodeWidths[node.style];
       nodeIndex++;
       maxDepth = Math.max(maxDepth, node.depth);
     });
-    for (var i = 0; i <= maxDepth; i++) {
-      nexts.push(0);
-    }
   }
 
-  function initTree(node, depth) {
-    var tree = {
-      id: node.masterId.substr(0, 4),
-      x: nexts[depth],
-      y: depth,
-      nodeIndex: node.nodeIndex,
-      mod: 0,
-      children: []
-    };
-    node.outgoingLinks.forEach(l => {
-      tree.children.push(buildTree(l.target, depth + 1));
-    });
-    return tree;
+  function areEqualNodes(v, w) {
+    return v.data.id === w.data.id;
   }
 
-  function setup(tree, depth, nexts, offset) {
-    var place = 0;
-    if (typeof (depth) === 'undefined') depth = 0;
-    if (typeof (nexts) === 'undefined') nexts = new defaultDict();
-    if (typeof (offset) === 'undefined') offset = new defaultDict();
-    for (var childIndex in tree.children) {
-      setup(tree.children[childIndex], depth + 1, nexts, offset);
-    }
-    tree.y = depth;
-    nodes[tree.nodeIndex].y = tree.y;
-    if (!tree.children.length) {
-      place = nexts.get(depth);
-      tree.x = place;
-    } else if (tree.children.length === 1) {
-      place = tree.children[0].x - 1;
-    } else {
-      place = (tree.children[0].x + tree.children[1].x) / 2;
-    }
-
-    offset[depth] = Math.max(offset.get(depth), nexts.get(depth) - place);
-    if (tree.children.length) {
-      tree.x = place + offset.get(depth);
-    }
-    nexts[depth] = nexts.get(depth) + 2;
-    tree.offset = offset.get(depth);
-
-    return tree;
-  }
-
-  function addmods(tree, modsum) {
-    if (typeof (modsum) === 'undefined') modsum = 0;
-    tree.x = tree.x + modsum;
-    nodes[tree.nodeIndex].x = tree.x;
-    modsum += tree.offset;
-
-    for (var childIndex in tree.children) {
-      addmods(tree.children[childIndex], modsum);
-    }
-    return tree;
-  }
-
-  function buildTree(node, depth) {
-    if (typeof (depth) === 'undefined') depth = 0;
-
-    var tree = {
-      id: node.masterId.substr(0, 4),
-      x: nexts[depth],
-      y: depth,
-      nodeIndex: node.nodeIndex
-    };
-    tree.children = [];
-    nodes[node.nodeIndex].x = tree.x;
-    nodes[node.nodeIndex].y = tree.y;
-    nexts[depth] += 1;
-    node.outgoingLinks.forEach(l => {
-      tree.children.push(buildTree(l.target, depth + 1));
-    });
-    return tree;
-  }
-
-  function computeNodeLinks() {
+  function prepareLinks() {
     links.forEach(function (link) {
       link.source = nodes.find(function (node) {
         return node.masterId === link.sourceId;
@@ -130,18 +247,9 @@ function graphDisplayService() {
       });
 
       link.source.outgoingLinks.push(link);
-      link.target.incomingLinks.push(link);
+        link.target.incomingLinks.push(link);
     });
   }
 
-  function defaultDict() {
-    this.get = function (key) {
-      if (this.hasOwnProperty(key)) {
-        return this[key];
-      } else {
-        return 0;
-      }
-    }
-  }
   return context;
 }
